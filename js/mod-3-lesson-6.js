cef0b39e20b33f9547ee7317266df15bd4b26270
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-01-13 00:48:36 +0200

    add task

---
+++ js/mod-3-lesson-6.js
@@ -0,0 +1,42 @@
+// ## Example 1 - Деструктуризація
+
+// Перепиши функцію так, щоб вона приймала один об'єкт параметрів замість набору
+// незалежних аргументів.
+
+// ```js
+function calcBMI({weight, height}) {
+  const numericWeight = Number(weight.replace(',', '.'));
+  const numericHeight = Number(height.replace(',', '.'));
+  return Number((numericWeight / numericHeight ** 2).toFixed(1));
+}
+
+const user = {
+  weight: '88,3',
+  height: '1.75',
+}
+
+// // Було
+// console.log(calcBMI('88,3', '1.75'));
+// // console.log(calcBMI('68,3', '1.65'));
+// // console.log(calcBMI('118,3', '1.95'));
+
+// // Очікується
+// console.log(
+//   calcBMI({
+//     weight: '88,3',
+//     height: '1.75',
+//   }),
+// );
+// console.log(
+//   calcBMI({
+//     weight: '68,3',
+//     height: '1.65',
+//   }),
+// );
+// console.log(
+//   calcBMI({
+//     weight: '118,3',
+//     height: '1.95',
+//   }),
+// );
+// ```
