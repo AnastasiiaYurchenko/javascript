28f768d8f3205eb94e6f9602b02a8056fe1e57e1
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-01-14 00:11:39 +0200

    add Artem's tasks

--- js/mod-3-lesson-6.js
+++ js/mod-3-lesson-6.js
@@ -40,3 +40,159 @@ const user = {
 //   }),
 // );
 // ```
+// ## Example 3 - Глибока деструктуризація
+
+// Перепиши функцію так, щоб вона приймала один об'єкт параметрів замість набору
+// незалежних аргументів.
+
+// ```js
+// function getBotReport(companyName, repairBots, defenceBots) {
+//   return `${companyName} has ${repairBots + defenceBots} bots in stock`;
+// }
+
+// // Було
+// console.log(getBotReport('Cyberdyne Systems', 150, 50));
+
+// // Очікується
+// console.log(getBotReport(boatCompany)); // "Cyberdyne Systems has 200 bots in stock"
+// ```
+// function getBotReport(boatCompany) {
+//   const {
+//     companyName,
+//     bots: {
+//       repair: repairBots = 0,
+//       defence: defenceBots = 0,
+//     }
+//   } = boatCompany;
+//   return `${companyName} has ${repairBots + defenceBots} bots in stock`;
+// }
+
+// aбо
+
+// function getBotReport({companyName, bots: {repair: repairBots = 0,
+//       defence: defenceBots = 0,}}) {
+//   return `${companyName} has ${repairBots + defenceBots} bots in stock`;
+// }
+
+// const boatCompany = {
+//   companyName: 'Cyberdyne Systems',
+//   bots: {
+//     repair: 250,
+//     defence: 50,
+//   },
+// }
+
+// console.log(getBotReport(boatCompany));
+
+// ## Example 4 - Деструктуризація
+
+// Напиши функцію, що приймає об'єкт параметрів із властивостями
+// `companyName` та `stock` та виводить звіт про кількість товарів на складі будь-якої
+// компанії.
+
+// function getStockReport({ companyName, stock }) {
+//   let stockAmount = 0;
+//   console.log(companyName);
+//   console.log(stock);
+
+//   const stockValues = Object.values(stock);
+//   console.log(stockValues);
+
+//   for (const value of stockValues) {
+//     stockAmount += value;
+//   }
+//   return ` ${companyName} has ${stockAmount} bots`
+// }
+
+// const company1 = {
+//   companyName: 'Cyberdyne Systems',
+//   stock: {
+//     repairBots: 150,
+//     defenceBots: 50,
+//     defenceBots2: 50,
+//     defenceBots1: 50,
+//   },
+// };
+
+// const report = getStockReport(company1);
+// console.log(report)
+
+// ## Example 5 - Операція spread
+
+// Доповни функцію `createContact(partialContact)` так, щоб вона повертала новий
+// об'єкт контакту з доданими властивостями `id` та `createdAt`, а також `list` зі
+// значенням "default" якщо в `partialContact` немає такої властивості
+// function createContact(partialContact) {
+//   return {
+//     list: 'default',
+//     ...partialContact,
+//     id: generateId(),
+//     createdAt: Date.now(),
+//   }
+// }
+
+// або деструктуризація
+
+// function createContact({name, email, list = 'default'}) {
+//   return {
+//     name, 
+//     email, 
+//     list,
+//     id: generateId(),
+//     createdAt: Date.now(),
+//   }
+// }
+
+// const contact1 = {
+//   name: 'Mango',
+//   email: 'mango@mail.com',
+//   list: 'friends',
+// }
+
+// const newContact = createContact(contact1);
+// console.log(newContact)
+
+// function generateId() {
+//   return '_' + Math.random().toString(36).substr(2, 9);
+// }
+
+// console.log(
+//   createContact({
+//     name: 'Mango',
+//     email: 'mango@mail.com',
+//     list: 'friends',
+//   }),
+// );
+// console.log(
+//   createContact({
+//     name: 'Poly',
+//     email: 'poly@hotmail.com',
+//   }),
+// );
+
+// ## Example 6 - Операція rest
+
+// Напиши функцію `transformUsername(user)` так, щоб вона повертала новий об'єкт із властивістю
+// `fullName`, замість `firstName` та `lastName`.
+
+function transformUserName({ firstName, lastName, ...otherProps }) {
+  return {
+    ...otherProps,
+    fullName: `${firstName} ${lastName}`,
+  }
+}
+
+const user1 = {
+  id: 1,
+  firstName: 'Jacob',
+  lastName: 'Mercer',
+  email: 'j.mercer@mail.com',
+  friendCount: 40,
+  friends: {
+    Anton: 'best',
+    Ivan: 'so so'
+  }
+}
+
+const newUser = transformUserName(user1);
+console.log(newUser)
