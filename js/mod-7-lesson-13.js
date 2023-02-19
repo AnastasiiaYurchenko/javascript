eb101f8ec1d7951c6ef30f248c6c487cc95c7b27
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-02-20 00:38:22 +0200

    add package.json

--- js/mod-7-lesson-13.js
+++ js/mod-7-lesson-13.js
@@ -1,7 +1,7 @@
-const imageContainer = document.querySelector('.images-gallery')
+// const imageContainer = document.querySelector('.images-gallery')
 
-const imgElements =
-images.map(item) => {
-    imageContainer.insertAdjacentHTML('beforeend',
-    '<img src="${item.src}" title="${item.title}" alt="${item.title}"/>')
-}
+// const imgElements =
+// images.map(item) => {
+//     imageContainer.insertAdjacentHTML('beforeend',
+//     '<img src="${item.src}" title="${item.title}" alt="${item.title}"/>')
+// }
